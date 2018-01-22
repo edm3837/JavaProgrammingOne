af4c6211baaa76ca8031a82db337eb0b781f6344
em0947744 <em0947744@RK03-B1225-13.francistuttle.edu>
2018-01-22 14:11:17 -0600

    Added program 7

---
+++ src/Chapter7/P7.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter7;
+
+/**
+ * Program takes an array and then finds the average in it
+ *
+ * @author em0947744
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P7 {
+
+    /**
+     * Main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] average;
+        int arraySize;
+
+        System.out.println("Please enter your array size: ");
+        arraySize = input.nextInt();
+        //Creates size of array based on user input
+        average = new double[arraySize];
+
+        arrayInputs(average, arraySize);
+
+        System.out.println("Your average is " + arrayAverage(average, arraySize));
+        System.out.println(Arrays.toString(average));
+    }
+
+    /**
+     * Returns the array and how big it is
+     *
+     * @param array
+     * @param arraySize
+     */
+    public static void arrayInputs(double[] array, int arraySize) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter " + arraySize + " variables: ");
+        for (int x = 0; x < arraySize; x++) {
+            array[x] = input.nextDouble();
+        }
+        arrayAverage(array, arraySize);
+    }
+
+    /**
+     * Finds the average in the array
+     *
+     * @param array
+     * @param arraySize
+     * @return
+     */
+    public static double arrayAverage(double[] array, double arraySize) {
+        double averages = 0;
+        for (int x = 0; x < arraySize; x++) {
+            averages += array[x];
+        }
+        averages = (averages / arraySize);
+        return averages;
+    }
+}
